e7dd7abd824b1e5a24751d74e01dc67d62e76fd4
rehana9398 <rehanashaik4099@gmail.com>
2025-02-12 10:35:07 +0530

    Add files via upload

---
+++ acendingorder.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Ascendingorder {
+	
+	public static void main(String []args)
+	{
+		int [] arr = new int[] {4,0,-9,2,-4,1,4,-3};
+		Arrays.sort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
